b44140871a0dc31924817bde547cfd27ac776c78
Celinafernandes9 <celina.922.fernz@gmail.com>
2024-02-28 13:26:02 +0530

    update

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -12,7 +12,7 @@ function Banner() {
         const request = await axios.get(requests.fetchNetflixOriginals);
         setMovie(
           request.data.results[
-            Math.floor(Math.random) * request.data.results.length - 1
+            Math.floor(Math.random()* request.data.results.length - 1) 
           ]
         );
         return request;
@@ -27,18 +27,14 @@ function Banner() {
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
-
-  const backgroundImage = movie?.backdrop_path
-    ? `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`
-    : "";
   return (
     <div>
       <header
         className="banner"
         style={{
           backgroundSize: "cover",
-          backgroundImage: backgroundImage,
-          backgroundPosition: "center center",
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: "center-center"
         }}
       >
         <div className="banner_contents">
